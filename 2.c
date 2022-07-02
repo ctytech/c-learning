f45133af60d33f5559cf90c3852d545be441be27
ctytech <96678150+ctytech@users.noreply.github.com>
2022-07-02 17:35:15 +1000

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+
+//计算数组中每个数的总和
+/*	int x, right, sum = 0;
+
+	scanf ("%i", &x);
+	while ( x != 0 ) {
+
+	right = x % 10;
+	sum = sum + right;
+	x = x / 10;
+
+	
+	}
+
+	printf ("%i\n", sum);
+*/
+//阶乘计算器
+/*	int ntime, number, n = 1;
+
+	scanf("%i", &number);
+	for ( ntime = 1; ntime <= number; ntime++)
+		n = n * ntime;
+
+	printf("%i\n", n);
+
+*/
+
+//平方计算器
+/*	int square, number, x;
+
+	printf ("Number list \n");
+	printf ("=========== \n");
+
+	for ( number = 1 ; number <= 10; number++) {
+
+	
+		square = number * number;
+		printf ("%i\n", square);
+	}
+*/
+
+
+//这个程序是计算三角形（可让用户输入）
+/*	int time;
+
+
+	printf("What times do you want ? ");
+	scanf("%i", &time);
+	for ( int counter = 1, number, cout; counter <= time; ++counter) {
+
+		printf ("What ");
+		scanf ("%i", &number);
+
+	
+
+	cout = 0;
+
+	for ( int num = 1; num <= number; num++ )
+		cout += num;
+
+	printf("%i\n", cout);
+
+	}
+*/
+return 0;
+}
